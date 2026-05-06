2e2ca4806fbb3a87c6384942e822de33693d3c1a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-06 13:53:49 +0200

    fix: change padding

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-black text-white w-full mt-4 py-3 border border-black']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-black text-white w-full mt-4 py-2 border border-black']) }}>
     {{ $slot }}
 </button>
