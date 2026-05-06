211f6959c034624a4ec58c27c0fc044df2badd3c
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-06 15:26:31 +0200

    fix: change placeholder text color

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] p-2 w-full']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] p-2 w-full placeholder-[#737373]']) }}>
