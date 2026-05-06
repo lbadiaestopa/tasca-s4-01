a8d88253734c7d723e27b96b4ac0a0779fac28f6
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-06 13:10:23 +0200

    feat: change styles

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'block text-[#1B1A1A]']) }}>
     {{ $value ?? $slot }}
 </label>
