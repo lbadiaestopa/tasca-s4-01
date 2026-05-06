e6988c9872b60c2ed6fa3f12aa17e3d728e4f80a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-06 13:57:43 +0200

    fix: change top margin

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-black text-white w-full mt-4 py-2 border border-black']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-black text-white w-full mt-2 py-2 border border-black']) }}>
     {{ $slot }}
 </button>
