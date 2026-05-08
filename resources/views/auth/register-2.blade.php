a8de7c49b875ff9595d3019ba8ff5bcd6cb4c916
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-08 17:16:00 +0200

    fix: change buttons to 'a' elements

--- resources/views/auth/register-2.blade.php
+++ resources/views/auth/register-2.blade.php
@@ -1,13 +1,10 @@
 <x-guest-layout>
-        <x-primary-button class="flex items-center justify-center mt-4">
-            <a href="{{ route('join-orchestra') }}">
-                {{ __('I have an orchestra code') }}
-            </a>
-        </x-primary-button>
+    <div class="flex flex-col col-span-4">
+        <a href="{{ route('join-orchestra') }}" class="border border-black bg-black text-white w-full mt-2 py-2 text-center">
+            {{ __('I have an orchestra code') }}
+        </a>
 
-        <x-secondary-button class="flex items-center justify-center mt-4">
-            <a href="{{ route('dashboard') }}">
-                {{ __('Create an administrator account') }}
-            </a>
-        </x-secondary-button>
+        <a href="{{ route('dashboard') }}" class="border border-black w-full py-2 mt-6 text-center">
+            {{ __('Create an administrator account') }}
+        </a></div>
 </x-guest-layout>
