d70689093003aaad240bc77504e0b5105fd29250
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-11 17:18:22 +0200

    feat: add new orchestra navitgation link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('orchestras.index')" :active="request()->routeIs('orchestras.index')" class="text-[#1B1A1A] leading-9">
+                        {{ __('Orchestra') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
