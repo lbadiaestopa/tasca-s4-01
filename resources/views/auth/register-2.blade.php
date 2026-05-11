6c28993963ffab734127e55538c8d2022b22725a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-11 12:31:53 +0200

    fix: change 'a' elements to forms

--- resources/views/auth/register-2.blade.php
+++ resources/views/auth/register-2.blade.php
@@ -1,10 +1,19 @@
 <x-guest-layout>
     <div class="flex flex-col col-span-4">
-        <a href="{{ route('join-orchestra') }}" class="border border-black bg-black text-white w-full mt-2 py-2 text-center">
-            {{ __('I have an orchestra code') }}
-        </a>
+        <form method="POST" action="{{ route('create-member-account') }}">
+            @csrf
 
-        <a href="{{ route('dashboard') }}" class="border border-black w-full py-2 mt-6 text-center">
-            {{ __('Create an administrator account') }}
-        </a></div>
+            <x-primary-button type="submit" class="border border-black w-full py-2 mt-6 text-center">
+                {{ __('Create a musician account') }}
+            </x-primary-button>
+        </form>
+
+        <form method="POST" action="{{ route('create-admin-account') }}">
+            @csrf
+
+            <x-secondary-button type="submit" class="border border-black w-full py-2 mt-6 text-center">
+                {{ __('Create an administrator account') }}
+            </x-secondary-button>
+        </form>
+    </div>
 </x-guest-layout>
