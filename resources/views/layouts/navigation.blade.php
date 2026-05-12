453122cad4be0e5a20319e6860b924992f15c9bc
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-12 16:45:36 +0200

    fix: change route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,8 +18,8 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('orchestra')" :active="request()->routeIs('orchestras')" class="text-[#1B1A1A] leading-9">
-                        {{ __('Orchestra') }}
+                    <x-nav-link :href="route('orchestras')" :active="request()->routeIs('orchestras')" class="text-[#1B1A1A] leading-9">
+                        {{ __('Orchestras') }}
                     </x-nav-link>
                 </div>
             </div>
