212ad791af6c21ab6440a8cb63d4d66e9983b8a2
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-12 19:20:43 +0200

    feat: add auth wrong routes and new orchestra routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,43 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OnboardingController;
+use App\Http\Controllers\OrchestraController;
+use App\Http\Middleware\EnsureOnboardingIsCompleted;
+use App\Http\Middleware\RoleMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('register-2', [OnboardingController::class, 'step2'])
+        ->name('register-2');
+
+    Route::post('create-member-account', [OnboardingController::class, 'createMemberAccount'])
+        ->name('create-member-account');
+
+    Route::post('create-admin-account', [OnboardingController::class, 'createAdminAccount'])
+        ->name('create-admin-account');
+
+    Route::middleware(EnsureOnboardingIsCompleted::class)->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
+        Route::middleware(RoleMiddleware::class)->group(function () {
+            Route::get('/orchestras', [OrchestraController::class, 'index'])->name('orchestras');
+
+            Route::get('/orchestras/{orchestra}', [OrchestraController::class, 'show'])->name('orchestras.show');
+
+            Route::get('/create-orchestra', fn() => view('orchestras.create'))->name('create-orchestra');
+            Route::post('create-orchestra', [OrchestraController::class, 'store']);
+        });
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
