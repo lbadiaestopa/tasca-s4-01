cfeec26ef776d0494cf22ffd43484b8c0274c5c6
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-13 15:55:26 +0200

    fix: remove nullable

--- database/migrations/2026_05_11_171138_create_programs_table.php
+++ database/migrations/2026_05_11_171138_create_programs_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->date('start_date');
             $table->date('end_date');
-            $table->foreignId('orchestra_id')->nullable();
+            $table->foreignId('orchestra_id');
         });
     }
 
