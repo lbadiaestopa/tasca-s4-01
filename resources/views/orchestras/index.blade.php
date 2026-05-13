0da4f1f5fe2856e80c631062ebe6d1fdd4d5b3bc
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-13 10:15:41 +0200

    feat: align elements vertically

--- resources/views/orchestras/index.blade.php
+++ resources/views/orchestras/index.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
     <a href="create-orchestra" class="absolute bottom-12 right-16 border border-black bg-black text-white px-4 py-2">+ Add an orchestra</a>
 
-    @foreach($orchestras as $orchestra)
-    <a href="{{ route('orchestras.show', $orchestra) }}">
-        {{ $orchestra->name }}
-    </a>
-    @endforeach
+    <div class="flex flex-col">
+        @foreach($orchestras as $orchestra)
+        <a href="{{ route('orchestras.show', $orchestra) }}">
+            {{ $orchestra->name }}
+        </a>
+        @endforeach
+    </div>
 </x-app-layout>
