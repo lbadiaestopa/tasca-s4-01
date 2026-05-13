cf15111edb0c48be2fdef61596261c6c59d8cdbd
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-13 11:32:06 +0200

    feat: add programs info and button to add programs

--- resources/views/orchestras/show.blade.php
+++ resources/views/orchestras/show.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <a href="create-program" class="absolute bottom-12 right-16 border border-black bg-black text-white px-4 py-2">+ Add a new program</a>
+
     <div class="flex">
         <h1>{{ $orchestra->name }}</h1>
 
@@ -10,4 +12,16 @@
             Edit
         </a>
     </div>
+
+    <h2>Programs</h2>
+
+    @foreach ($orchestra->programs as $program)
+    <div>
+        <h3>{{ $program->title }}</h3>
+
+        <p>{{ $program->start_date }}</p>
+
+        <p>{{ $program->end_date }}</p>
+    </div>
+    @endforeach
 </x-app-layout>
