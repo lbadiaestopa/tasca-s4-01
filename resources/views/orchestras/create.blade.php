d025f2ff16b06e16afb7719959862958c47c0e20
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-14 17:08:10 +0200

    feat: add back button and title for accessibility

--- resources/views/orchestras/create.blade.php
+++ resources/views/orchestras/create.blade.php
@@ -1,6 +1,15 @@
 <x-app-layout>
     <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-x-4 md:gap-x-6 w-full mt-20">
-        <div class="col-span-4 md:col-start-3 lg:col-start-5 flex justify-center">
+        <div class="col-span-4 md:col-start-3 lg:col-start-5 flex justify-between items-end mt-6">
+            <a href="orchestras">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="#000000">
+                    <path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z" /></svg>
+            </a>
+            <h2 class="font-semibold text-center">Create orchestra</h2>
+            <div class="h-6 w-6"></div>
+        </div>
+
+        <div class="col-span-4 md:col-start-3 lg:col-start-5 flex justify-center mt-4">
             <form class="w-full" method="POST" action="{{ route('orchestras.create') }}">
                 @csrf
 
@@ -13,14 +22,14 @@
 
                 <!-- City -->
                 <div class="mt-2">
-                    <x-input-label for="city" :value="__('City')" class="ms-2"/>
+                    <x-input-label for="city" :value="__('City')" class="ms-2" />
                     <x-text-input id="city" class="block mt-1 w-full" type="text" name="city" placeholder="e. g. Barcelona" :value="old('city')" required autocomplete="address-level2" />
                     <x-input-error :messages="$errors->get('city')" class="mt-2" />
                 </div>
 
                 <!-- Venue -->
                 <div class="mt-2">
-                    <x-input-label for="venue" :value="__('Venue')" class="ms-2"/>
+                    <x-input-label for="venue" :value="__('Venue')" class="ms-2" />
                     <x-text-input id="venue" class="block mt-1 w-full" type="text" name="venue" placeholder="e. g. L'Auditori" :value="old('venue')" required autocomplete="organization" />
                     <x-input-error :messages="$errors->get('venue')" class="mt-2" />
                 </div>
@@ -31,7 +40,6 @@
                         {{ __('Create') }}
                     </x-primary-button>
                 </div>
-
             </form>
         </div>
     </div>
