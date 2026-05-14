c7a1669bf3a55929d8571c4bd96f16c40493f459
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-14 12:44:27 +0200

    feat: remove outline when focused

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] placeholder-[#737373] rounded-2xl w-full py-3']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] placeholder-[#737373] rounded-2xl w-full py-3 focus:border-[#D9D9D9] focus:ring-0 focus:outline-none']) }}>
