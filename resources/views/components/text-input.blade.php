3c92472b41a4ba3d884344b03e56a6f9c601fa62
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-14 10:26:58 +0200

    feat: modify text input styles

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] p-2 w-full placeholder-[#737373]']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border border-[#D9D9D9] placeholder-[#737373] rounded-2xl w-full p-4']) }}>
