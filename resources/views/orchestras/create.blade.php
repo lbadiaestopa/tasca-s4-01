25f69d6eddeacd8e03c667cf58c6e643376c19f0
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-14 13:13:45 +0200

    feat: change  styles

--- resources/views/orchestras/create.blade.php
+++ resources/views/orchestras/create.blade.php
@@ -6,27 +6,27 @@
 
                 <!-- Name -->
                 <div>
-                    <x-input-label for="name" :value="__('Name')" />
+                    <x-input-label for="name" :value="__('Name')" class="ms-2" />
                     <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="e. g. OBC" :value="old('name')" required autofocus autocomplete="name" />
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
 
                 <!-- City -->
-                <div class="mt-4">
-                    <x-input-label for="city" :value="__('City')" />
+                <div class="mt-2">
+                    <x-input-label for="city" :value="__('City')" class="ms-2"/>
                     <x-text-input id="city" class="block mt-1 w-full" type="text" name="city" placeholder="e. g. Barcelona" :value="old('city')" required autocomplete="address-level2" />
                     <x-input-error :messages="$errors->get('city')" class="mt-2" />
                 </div>
 
                 <!-- Venue -->
-                <div class="mt-4">
-                    <x-input-label for="venue" :value="__('Venue')" />
+                <div class="mt-2">
+                    <x-input-label for="venue" :value="__('Venue')" class="ms-2"/>
                     <x-text-input id="venue" class="block mt-1 w-full" type="text" name="venue" placeholder="e. g. L'Auditori" :value="old('venue')" required autocomplete="organization" />
                     <x-input-error :messages="$errors->get('venue')" class="mt-2" />
                 </div>
 
                 <!-- Submit -->
-                <div class="mt-6">
+                <div class="mt-4">
                     <x-primary-button>
                         {{ __('Create') }}
                     </x-primary-button>
