af79a9499f745e5367f7d083f528149710ae76e5
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-14 17:07:10 +0200

    feat: change styles

--- resources/views/orchestras/show.blade.php
+++ resources/views/orchestras/show.blade.php
@@ -1,29 +1,30 @@
 <x-app-layout>
-    <a href="{{ route('program.create', $orchestra) }}" class="absolute bottom-12 right-16 border border-black bg-black text-white px-4 py-2">+ Add a new program</a>
-
-    <div class="flex">
-        <h1>{{ $orchestra->name }}</h1>
-
-        <p>{{ $orchestra->city }}</p>
-
-        <p>{{ $orchestra->venue }}</p>
+    <div class="flex mt-6 justify-between">
+        <h1 class="font-semibold text-2xl">{{ $orchestra->name }}</h1>
 
         <a href="{{ route('orchestras.edit', $orchestra) }}">
-            Edit
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+                <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg>
         </a>
     </div>
 
-    <h2>Programs</h2>
+    <div class="flex-1 h-px bg-black"></div>
+
+    <h2 class="mt-6">Next programs</h2>
 
     @foreach ($orchestra->programs as $program)
     <a href="{{ route('programs.show', [$orchestra, $program]) }}">
-        <div class="border border-black">
-            <h3>{{ $program->name }}</h3>
+        <div class="py-3 mt-4 border border-[#D9D9D9] rounded-2xl">
+            <div class="flex gap-8 ms-6">
+                <h3 class="font-semibold">{{ $program->name }}</h3>
 
-            <p>{{ $program->start_date }}</p>
+                <p>{{ $program->start_date }}</p>
 
-            <p>{{ $program->end_date }}</p>
+                <p>{{ $program->end_date }}</p>
+            </div>
         </div>
     </a>
     @endforeach
+
+    <a href="{{ route('program.create', $orchestra) }}" class="absolute bottom-12 right-8 md:right-12 border border-[#D9D9D9] bg-white shadow-md rounded-2xl px-6 py-3 hover:scale-110 transition-transform duration-200">+ Add a new program</a>
 </x-app-layout>
