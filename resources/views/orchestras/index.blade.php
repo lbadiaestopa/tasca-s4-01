182adbe105e54f72b256381e02b5105900833944
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-17 17:34:13 +0200

    feat: redone structure to become main page

--- resources/views/orchestras/index.blade.php
+++ resources/views/orchestras/index.blade.php
@@ -1,14 +1,37 @@
 <x-app-layout>
+    <div class="flex min-h-screen gap-6">
+
+        <aside class="w-80 mt-4 ml-4">
+            <x-orchestra-tree :orchestras="$orchestras" />
+        </aside>
+
+        <main class="flex-1 mt-4 mr-4">
+            <h2 class="col-span-full text-xl leading-9 mb-2">Next Events</h2>
+
+            @foreach($events as $event)
+            <a href="{{ route('events.show', ['orchestra' => $event->program->orchestra, 'program' => $event->program, 'event' => $event]) }}">
+                <div class="mb-2 block p-2 rounded-lg bg-[#FAFAFA]">
+                    <div>
+                        <h3 class="font-semibold">{{ $event->name }}</h3>
+                        <a href="events.edit"></a>
+                    </div>
+                    <div class="flex justify-between">
+                        <div class="flex gap-2">
+                            <p class="text-[#737373]">{{ $event->program->name }}</p>
+                            <p class="text-[#737373]">·</p>
+                            <p class="text-[#737373]">{{ $event->program->orchestra->name }}</p>
+                        </div>
+
+                        <div class="flex gap-4">
+                            <p class="text-[#737373]">{{ $event->start_date->format('H:i') }} - {{ $event->end_date->format('H:i') }}</p>
+                            <p>{{ $event->start_date->format('d/m/y') }}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            @endforeach
+        </main>
+    </div>
+
     <a href="create-orchestra" class="absolute bottom-12 right-8 md:right-12 border border-[#D9D9D9] bg-white shadow-md rounded-2xl px-6 py-3 hover:scale-110 transition-transform duration-200">+ Add an orchestra</a>
-    @foreach($orchestras as $orchestra)
-    <a href="{{ route('orchestras.show', $orchestra) }}">
-        <div class="py-3 px-6 mt-4 border border-[#D9D9D9] rounded-2xl">
-            <div class="flex gap-8">
-                <h3 class="font-semibold">{{ $orchestra->name }}</h3>
-                <p class="text-[#737373]">{{ $orchestra->venue }}</p>
-                <p class="text-[#737373]">{{ $orchestra->city }}</p>
-            </div>
-        </div>
-    </a>
-    @endforeach
 </x-app-layout>
