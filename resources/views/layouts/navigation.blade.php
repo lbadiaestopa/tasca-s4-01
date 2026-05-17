04705aa77dc915f51e50acd30c41482f46ec90a1
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-17 17:33:41 +0200

    feat: remove unnecessary dashboard link and change logo route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,18 +5,12 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('orchestras') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-[#1B1A1A]" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-[#1B1A1A] leading-9">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('orchestras')" :active="request()->routeIs('orchestras')" class="text-[#1B1A1A] leading-9">
                         {{ __('Orchestras') }}
