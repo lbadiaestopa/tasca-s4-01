c7252ec8659423a2994ac347ad57f22592d21210
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-18 11:11:25 +0200

    fix: change text to unify with other views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 -960 960 960" width="1.5rem" fill="#000000">
                 <path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z" /></svg>
         </a>
-        <h2 class="font-semibold text-center">Sign In to +++</h2>
+        <h2 class="font-semibold text-center">Sign Up To +++</h2>
         <div class="h-6 w-6"></div>
     </div>
 
@@ -31,20 +31,18 @@
 
         <!-- Password -->
         <div class="mt-2">
-            <x-text-input id="password" class="block w-full" placeholder="Password (at least 8 charecters)" type="password" name="password" required autocomplete="new-password" />
-
+            <x-text-input id="password" class="block w-full" placeholder="Password (at least 8 characters)" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-2">
             <x-text-input id="password_confirmation" class="block w-full" placeholder="Repeat the Password" type="password" name="password_confirmation" required autocomplete="new-password" />
-
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <x-primary-button class="mt-4">
-            {{ __('Next') }}
+            {{ __('Sign Up') }}
         </x-primary-button>
     </form>
 </x-guest-layout>
