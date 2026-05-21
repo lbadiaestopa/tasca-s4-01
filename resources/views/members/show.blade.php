83717b361a376f208ef48469025ee98279039622
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-21 10:58:30 +0200

    feat: add edit button

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -6,7 +6,13 @@
 
         <div class="flex flex-col w-full mx-4">
             <div class="flex flex-col mt-6 justify-between">
-                <h1 class="font-semibold text-2xl">{{ $membership->user->name }} {{ $membership->user->last_name }}</h1>
+                <div class="flex justify-between">
+                    <h1 class="font-semibold text-2xl">{{ $membership->user->name }} {{ $membership->user->last_name }}</h1>
+
+                    <div class="flex gap-2">
+                        <a href="{{ route('memberships.edit', [$orchestra, $membership]) }}" class="px-4 py-1 border rounded-xl">Edit</a>
+                    </div>
+                </div>
 
                 <div class="flex gap-8">
                     <p>
