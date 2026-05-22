e6da445e1e1a1cec539e15fbc296856cd076593e
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-22 13:37:15 +0200

    refactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,21 +15,19 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="min-h-dvh font-geist">
-    @include('layouts.navigation')
+<body class="h-dvh flex flex-col font-geist">
+    <!-- Page Heading -->
+    @isset($header)
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+    @endisset
 
-        <!-- Page Heading -->
-        @isset($header)
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-        @endisset
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+    <!-- Page Content -->
+    <main class="flex-1 min-h-0">
+        {{ $slot }}
+    </main>
 </body>
 </html>
