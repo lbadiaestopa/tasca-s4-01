b7b28ec82f704ea19872f3fc6863c7dd47269302
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-26 10:59:16 +0200

    fix: change delete account buttons to danger buttons

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -9,10 +9,10 @@
         </p>
     </header>
 
-    <x-primary-button
+    <x-danger-button class=""
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-primary-button>
+    >{{ __('Delete Account') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
@@ -46,9 +46,9 @@
                     {{ __('Cancel') }}
                 </x-secondary-button>
 
-                <x-primary-button class="ms-3 mt-6">
+                <x-danger-button class="ms-3 mt-6 bg-red-500">
                     {{ __('Delete Account') }}
-                </x-primary-button>
+                </x-danger-button>
             </div>
         </form>
     </x-modal>
