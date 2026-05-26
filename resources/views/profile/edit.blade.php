0b5d0b275bd275f5f6ef9419a9bda2b8f8f1210d
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-05-26 10:58:50 +0200

    fix: center forms

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-    <div class="py-12 col-span-4">
-        <div class="max-w-7xl mx-auto space-y-6">
-            <div class="max-w-xl">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto flex flex-col items-center space-y-6">
+            <div class="max-w-xl w-full">
                 @include('profile.partials.update-profile-information-form')
             </div>
 
-            <div class="max-w-xl">
+            <div class="max-w-xl w-full">
                 @include('profile.partials.update-password-form')
             </div>
 
-            <div class="max-w-xl">
+            <div class="max-w-xl w-full">
                 @include('profile.partials.delete-user-form')
             </div>
         </div>
